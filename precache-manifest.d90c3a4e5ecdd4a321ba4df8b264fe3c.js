3d646843048810e3c29ce9cf798ae94bc5a40b52
Zaya <zayazaw67@gmail.com>
2019-11-11 20:08:44 -0600

    Updates

---
+++ precache-manifest.d90c3a4e5ecdd4a321ba4df8b264fe3c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94c76dcb6fa0dce4192679f83ed7e791",
+    "url": "/Clicky-React/index.html"
+  },
+  {
+    "revision": "116f107484a99afc7ecb",
+    "url": "/Clicky-React/static/css/main.7a856265.chunk.css"
+  },
+  {
+    "revision": "41028ac793f2464c4092",
+    "url": "/Clicky-React/static/js/2.75e67943.chunk.js"
+  },
+  {
+    "revision": "116f107484a99afc7ecb",
+    "url": "/Clicky-React/static/js/main.5850efb8.chunk.js"
+  },
+  {
+    "revision": "c2944d48648eab93fdf3",
+    "url": "/Clicky-React/static/js/runtime-main.712bdc42.js"
+  }
+]);
