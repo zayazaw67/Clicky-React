64e5f475fe507bb68ffa8b68a4e87be577a0d53d
Zaya <zayazaw67@gmail.com>
2019-11-11 19:56:01 -0600

    changed server

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 
-const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -11,10 +10,9 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
-// Add routes, both API and view
-app.use(routes);
 
-// Start the API server0
+
+// Start the API server
 app.listen(PORT, function () {
     console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
